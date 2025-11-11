dc0de05bc9031137b71d0e35e12d0cd7f8d93b66
victormaximinodesouza <victormaximino18@gmail.com>
2025-11-11 20:41:25 -0300

    Adicionando um projeto

---
+++ js/app.js
@@ -0,0 +1,7 @@
+function comprar(){
+    let tipo = document.getElementById('tipo-ingresso');
+      let qtd = document.getElementById('qtd');
+
+      alert(qtd.value);
+      alert(tipo.value);
+}
