b56afad51554869cfe2130f3de4db216fc8c9c80
victormaximinodesouza <victormaximino18@gmail.com>
2025-11-11 21:26:41 -0300

    Finalizando

--- js/app.js
+++ js/app.js
@@ -1,7 +1,48 @@
 function comprar(){
     let tipo = document.getElementById('tipo-ingresso');
-      let qtd = document.getElementById('qtd');
+    let qtd = parseInt(document.getElementById('qtd').value);
 
-      alert(qtd.value);
-      alert(tipo.value);
+    if(tipo.value == 'pista'){
+        comprarPista(qtd); // apenas o valor digitado na string
+    } else if(tipo.value == 'Superior'){
+         comprarSuperior(qtd);
+    } else{
+         comprarInferior(qtd);
+    }
+    }
+function comprarPista(qtd){
+        let qtdPista = parseInt(document.getElementById('qtd-pista').textContent);
+        if(qtd > qtdPista){
+            alert('Quantidade indisponível para tipo Pista');
+        } else{
+            qtdPista = qtdPista - qtd;
+            document.getElementById('qtd-pista').textContent = qtdPista; // mexe no texto que está dentro da tag
+            alert('Compra realizada!');
+        }
+    }
+ function comprarSuperior(qtd){
+        let qtdSuperior = parseInt(document.getElementById('qtd-superior').textContent);
+        if(qtd > qtdSuperior){
+            alert('Quantidade indisponível para tipo Superior');
+        } else{
+            qtdSuperior = qtdSuperior - qtd;
+            document.getElementById('qtd-superior').textContent = qtdSuperior; // mexe no texto que está dentro da tag
+            alert('Compra realizada!');
+        }
+
+
+
+}
+
+ function comprarInferior(qtd){
+        let qtdInferior = parseInt(document.getElementById('qtd-inferior').textContent);
+        if(qtd > qtdInferior){
+            alert('Quantidade indisponível para tipo inferior');
+        } else{
+            qtdInferior = qtdInferior - qtd;
+            document.getElementById('qtd-inferior').textContent = qtdInferior; // mexe no texto que está dentro da tag
+            alert('Compra realizada!');
+        }
+
+
 }
